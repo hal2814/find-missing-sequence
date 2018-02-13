0b76f597a909396811974fcade8979d3113a8e16
Nate McGregor <nate@planetargon.com>
2018-02-13 15:17:05 -0800

    script larger arrays to show process

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 
+def list_missing_numbers(array)
+end
+
+def create_array_of_size(num)
+  ar = []
+  until num == 0
+    ar.push(num)
+    num -= 1
+  end
+  ar.reverse
+end
 
 # returns the sum of all numbers that come before num: ex. 3 => 6
 def triangular_number(ar)
@@ -31,9 +42,16 @@ def find_missing_number(array)
 end
 
 
-thear = [1,2,3,5,6,7,8,9,10,11,13,14,15]
+# thear = [1,2,3,5,6,7,8,9,10,11,13,14,15]
 
-puts "* * * * * * * *"
+thear = create_array_of_size(520)
+thear.delete(37)
+thear.delete(127)
+thear.delete(462)
+
+puts "********************"
+puts "**** FIRST RUN ****"
+puts "********************"
 
 puts "starting array is: "
 puts thear.to_s
@@ -55,7 +73,9 @@ puts "adding number to sequence..."
 puts "array is now: "
 puts thear.to_s
 
-puts "**** NEXT RUN ****"
+puts "********************"
+puts "**** SECOND RUN ****"
+puts "********************"
 
 puts "starting array is: "
 puts thear.to_s
@@ -69,10 +89,34 @@ puts count_triangular_number(thear.length)
 puts "missing number in the sequence is: "
 num = find_missing_number(thear)
 puts num
-thear.push(num)
+thear = thear.push(num).sort
 
 puts "----------------------------"
 puts "adding number to sequence..."
 
 puts "array is now: "
-puts thear.sort.to_s
+puts thear.to_s
+
+puts "********************"
+puts "**** THIRD RUN ****"
+puts "********************"
+
+puts "starting array is: "
+puts thear.to_s
+
+puts "array's triangular number is: "
+puts triangular_number(thear)
+
+puts "array count triangular number is: "
+puts count_triangular_number(thear.length)
+
+puts "missing number in the sequence is: "
+num = find_missing_number(thear)
+puts num
+thear = thear.push(num).sort
+
+puts "----------------------------"
+puts "adding number to sequence..."
+
+puts "array is now: "
+puts thear.to_s
